6322382226a3217ee4ef195043e79bc2d41b3775
Student <lanasigalova@gmail.com>
2024-06-15 21:40:44 -0400

    commit from console

---
+++ settings.gradle.kts
@@ -0,0 +1,2 @@
+rootProject.name = "first_hw_qa_guru_gitHub"
+
---
+++ src/test/java/ExampleTest.java
@@ -0,0 +1,9 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class ExampleTest {
+    @Test
+    void exampleTest(){
+        Assertions.assertTrue(2>1);
+    }
+}
